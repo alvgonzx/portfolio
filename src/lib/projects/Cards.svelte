869e631f1fc9b260fea309ca2c0f4afe2da7bb14
alvgonzx <alvgonz.ojeda@gmail.com>
2023-07-06 12:30:53 +0200

    [dev] updated links

--- src/lib/projects/Cards.svelte
+++ src/lib/projects/Cards.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div id="cards" class="flex w-full xl:flex-row xl:justify-between flex-col justify-center items-center mt-4 gap-5 xl:gap-0">
-    <Card file="1" name="Efteling Queue Checker" description="Efteling themepark queue checker built with Svelte, Vite and Tailwind using the queue-times.com API" /> 
-    <Card file="2" name="Ultimate GPT Writer" description="ChatGPT writer built with Python, Flask, Bootstrap and the OpenAI API. It's not free, it costs $12" /> 
-    <Card file="3" name="entresemillas.com Webscraper" description="Online store webscraper built with Python and BeautifulSoup. Hosted on Google Colab"/> 
+    <Card file="1" name="Efteling Queue Checker" url="https://efteling-queue-checker.vercel.app" description="Efteling themepark queue checker built with Svelte, Vite and Tailwind using the queue-times.com API" /> 
+    <Card file="2" name="Ultimate GPT Writer" description="ChatGPT writer built with Python, Flask, Bootstrap and the OpenAI API. It's not free, it costs $12" url="https://forobeta.com/temas/creador-de-articulos-automatico-desde-12.953909/#post-8637312" /> 
+    <Card file="3" name="entresemillas.com Webscraper" description="Online store webscraper built with Python and BeautifulSoup. Hosted on Google Colab" url="https://colab.research.google.com/drive/1VoVdRVSNXZDC-RfFos6moCbYB2xvUjuQ?usp=sharing"/> 
 </div>
