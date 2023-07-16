df5bfe6138876e6faf722508ee4932b2d54df390
Álvaro González <alvgonz.ojeda@gmail.com>
2023-07-16 19:02:49 +0200

    [deploy] preparing to deploy at gh-pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,10 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			runtime: 'nodejs18.x'
-		})
+		adapter: adapter()
 	},
 	preprocess: vitePreprocess()
 };
