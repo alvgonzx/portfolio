fc350db99de84a0cbc439894b5f5db6f0a8965c1
Álvaro González <alvgonz.ojeda@gmail.com>
2023-07-16 11:54:05 +0200

    [test] adding test blog

---
+++ portfolio/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import Header from '../lib/Header.svelte';
+	import Hero from '../lib/hero/Hero.svelte';
+	import AboutMe from '../lib/about-me/AboutMe.svelte';
+	import Projects from '../lib/projects/Projects.svelte';
+	import Footer from '../lib/Footer.svelte';
+</script>
+
+<main>
+	<Header />
+
+	<Hero />
+
+	<AboutMe />
+
+	<Projects />
+
+	<Footer />
+</main>
