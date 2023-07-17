b50958508fcc31acc7782b66b7bb763b2636f99b
Álvaro González <alvgonz.ojeda@gmail.com>
2023-07-17 11:13:00 +0200

    [deploy] buyed alvgonzx.site domain

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <header class="flex flex-wrap items-center justify-between px-[4%] mx-auto py-2 sticky top-0 z-10 bg-white border-b-2">
-    <a href="https://alvgonzx.vercel.app" class="text-2xl font-semibold flex items-center">alvgonzx</a>
+    <a href="https://alvgonzx.site" class="text-2xl font-semibold flex items-center">alvgonzx</a>
     <input type="checkbox" id="menu-toggle" class="hidden">
     <label for="menu-toggle" class="text-2xl cursor-pointer md:hidden">&#9776;</label>
     <nav class="flex flex-col md:flex-row gap-8 items-center w-full md:w-auto" id="menu">
--- src/lib/components/projects/Card.svelte
+++ src/lib/components/projects/Card.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <article class="xl:w-1/4 w-full border-[3px] rounded-md text-center p-8">
-    <img src="/portfolio/{file}.png" alt="{file}">
+    <img src="/{file}.png" alt="{file}">
     <h4 id="title" class="pt-4 font-medium text-[18px]">{name}</h4>
     <p class="mb-4">{description}</p>
     <a href="{url}" class="border-[3.5px] leading-none py-2 px-4 font-medium rounded-full border-[#3820CA] hover:bg-[#3820CA] hover:text-white transition-all duration-250 ease-in-out">Project details</a>
